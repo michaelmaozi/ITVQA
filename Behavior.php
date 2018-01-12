9b3f3e9f5551b79ca8708c57a479a0d637b07e84
Mao ZiXiang <>
2018-01-11 22:28:16 -0500

    updated behavior Questions

---
+++ Behavior.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+* 
+*/
+class ClassName
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	public function What_type_of_thinker_Macro_or_micro()
+	{
+
+	}
+
+	public function What_is_your_greatest_weakness()
+	{
+
+	}
+
+	public function What_is_your_favorite_technology_related_websites()
+	{
+
+	}
+
+	public function What_is_your_strengths_and_weakness()
+	{
+
+	}
+
+	public function what_arethe_three_majorissues_facing_the_provinces_health_care_system()
+	{
+
+	}
+
+	public function why_do_you_want_to_work_here()
+	{
+
+	}
+
+	public function what_are_you_post_proud_of()
+	{
+
+	}
+
+	public function where_do_you_see_yourself_in_3_to_5_years()
+	{
+
+	}
+
+	public function what_is_your_communication_style()
+	{
+
+	}
+
+	public function what_are_your_presentation_skills_like()
+	{
+
+	}
+
+	public function what_are_your_group_work_skills/style_like()
+	{
+
+	}
+
+	public function why_did_you_leave_your_previous_position/why_would_you()
+	{
+
+	}
+
+	public function what_are_the_3_main_thins_you_have_learned_in_your_previous/current_role()
+	{
+
+	}
+
+	public function what_do_you_like_to_do_for_fun()
+	{
+
+	}
+
+	public function what_organizational_tools_do_you_use_to_keep_organized()
+	{
+
+	}
+
+
+
+
+
+}
