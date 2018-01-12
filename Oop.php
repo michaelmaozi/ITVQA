2534c151893757100bb1e301f0de91e2c979b684
Dev-michael <michael@beapartof.com>
2018-01-12 13:16:02 -0500

    updated oops qa

--- Oop.php
+++ Oop.php
@@ -1,1 +1,30 @@
 <?php
+
+/**
+*  Oop Questions
+*/
+class ClassName
+{
+	public function What is Object Oriented Programming()
+	{
+		$a = array(
+			"1" => "oop is Programming paradiam",
+			"2" => "completed software operates as a bunch of objects talking to each other"
+			"3" => "An Object is a collection of data and methods that operate on its data"
+		);
+	}
+
+	public function Why oop()
+	{
+		"1" => "better manageable",
+		"2" => "The overall understanding of the software is increased as the distance between the language spoken by developers and that spoken by users",
+		"3" => "Object orientation eases maintenance by the use of encapsulation.   One can easily change the underlying representation by keeping the methods same."
+	}
+
+	public function What are main features of OOP()
+	{
+		"1" => "encapsulation",
+		"2" => "polymorphism",
+		"3" => "inheritance"
+	}
+}
