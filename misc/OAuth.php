1b318ed029a8c73f150be16e269cb80a3997f449
Dev-michael <michael@beapartof.com>
2018-01-19 17:51:20 -0500

    add oauth

---
+++ misc/OAuth.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+* 
+*/
+class ClassName extends AnotherClass
+{
+	
+	function What Is OAuth()
+	{
+		"1" => "OAuth is a standard that applications (and the developers who love them) can use to provide client applications with “secure delegated access”. OAuth works over HTTP and authorizes Devices, APIs, Servers and Applications with access tokens rather than credentials",
+		"2" => "There are two versions of OAuth: OAuth 1.0a and OAuth2. These specifications are completely different from one another, and cannot be used together: there is no backwards compatibility between them",
+		"3" => "OAuth is basically a protocol that supports authorization workflows. What this means is that it gives you a way to ensure that a specific user has permissions to do something."
+	}
+
+	function What Does OAuth Do()
+	{
+		"1" => "OAuth is basically a protocol that supports authorization workflows. What this means is that it gives you a way to ensure that a specific user has permissions to do something.",
+		"2" => "OAuth isn’t meant to do stuff like validate a user’s identity — that’s taken care of by an Authentication service. Authentication is when you validate a user’s identity (like asking for a username / password to log in), whereas authorization is when check to see what permissions an existing user already has"
+
+	}
+
+	function How OAuth Works()
+	{
+		"1" => 
+	}
+}
